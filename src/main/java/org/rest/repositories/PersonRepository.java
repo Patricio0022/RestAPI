9552bf331c61ede3b851f5a7ae93294776257d23
Patricio0022 <niellucsx@hotmail.com>
2024-09-06 23:23:31 -0300

    Added JPA-Hibernate Repository

---
+++ src/main/java/org/rest/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package org.rest.repositories;
+
+import org.rest.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+}
