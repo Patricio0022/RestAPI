03317f741e61d97e4e7090d4da06bff886b48f56
patricio0022 <niellucsx@hotmail.com>
2024-09-28 12:39:07 -0300

    .

--- src/main/java/org/rest/controller/PersonController.java
+++ src/main/java/org/rest/controller/PersonController.java
@@ -7,17 +7,11 @@ import org.rest.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/person/v1")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 	
 	@Autowired
