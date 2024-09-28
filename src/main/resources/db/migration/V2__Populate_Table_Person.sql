7adcb131e3cd065cdd4c8afa00adf9fe5a570b3f
patricio0022 <niellucsx@hotmail.com>
2024-09-28 12:42:39 -0300

    a

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `adress`, `gender`) VALUES
+	(1, 'Pinguim','Madagascar','Sao Paulo','Male'),
+	(2, 'Joao','Matos', 'Manaus','Male'),
+	(3, 'Antonio','Costa', 'Rio de Janeiro','Male'),
+	(4, 'Xuxa','Menedes', 'Rio de Janeiro','Female'),
+	(5, 'Maria ','Santos', 'Curitiba','Female');
+
+
