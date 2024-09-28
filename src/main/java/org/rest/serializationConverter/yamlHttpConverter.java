d0850be6b2d67ea60ff894a7daf249ac4ec4d8e8
patricio0022 <niellucsx@hotmail.com>
2024-09-28 18:44:58 -0300

    converter yaml

---
+++ src/main/java/org/rest/serializationConverter/yamlHttpConverter.java
@@ -0,0 +1,18 @@
+package org.rest.serializationConverter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+public class yamlHttpConverter extends AbstractJackson2HttpMessageConverter {
+
+//consumes/produces in yaml
+    public yamlHttpConverter() {
+        super(new YAMLMapper().
+                setSerializationInclusion(
+                    JsonInclude.Include.NON_NULL),
+                MediaType.parseMediaType("application/x-yaml"));
+    }
+}
