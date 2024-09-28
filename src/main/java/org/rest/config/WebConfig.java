9effbd24297126c82af7aeaee59de04e31177ca4
patricio0022 <niellucsx@hotmail.com>
2024-09-28 14:12:39 -0300

    new webconfig

--- src/main/java/org/rest/config/WebConfig.java
+++ src/main/java/org/rest/config/WebConfig.java
@@ -1,4 +1,26 @@
 package org.rest.config;
 
-public class WebConfig {
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        //choose response via QueryParam http://localhost:8080/api/person/v1?mediaType=xml
+
+        configurer.favorParameter(true)
+                .parameterName("medyaType").ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                    .mediaType("json", MediaType.APPLICATION_JSON)
+                    .mediaType("xml", MediaType.APPLICATION_XML);
+
+
+
+    }
 }
