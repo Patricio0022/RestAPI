ddbe0604e58dd314e81b70c1301275a7bf147478
patricio0022 <niellucsx@hotmail.com>
2024-10-02 20:11:25 -0300

    not is static

--- src/main/java/org/rest/mapper/DozerMapper.java
+++ src/main/java/org/rest/mapper/DozerMapper.java
@@ -10,11 +10,11 @@ public class DozerMapper {
 	//conversao de entity for VO
 	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 	
-	public static <O, D> D parseObject(O origin, Class<D> destination) {
+	public  <O, D> D parseObject(O origin, Class<D> destination) {
 		return mapper.map(origin, destination);
 	}
 	
-	public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
+	public  <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
 		List<D> destinationObjects = new ArrayList<D>();
 		for (O o : origin) {
 			destinationObjects.add(mapper.map(o, destination));
