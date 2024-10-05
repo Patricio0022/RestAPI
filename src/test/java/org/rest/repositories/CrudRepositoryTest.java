c6dbd4f796f563d25cbad5b24177c2bf14f59884
patricio0022 <niellucsx@hotmail.com>
2024-10-05 19:17:24 -0300

    added others tests

--- src/test/java/org/rest/repositories/CrudRepositoryTest.java
+++ src/test/java/org/rest/repositories/CrudRepositoryTest.java
@@ -21,7 +21,7 @@ import static org.mockito.Mockito.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension.class)
-class PersonRepositoryTest {
+class CrudRepositoryTest {
 
     @InjectMocks
     private PersonServices personServices; //real injection
@@ -31,7 +31,7 @@ class PersonRepositoryTest {
     private DozerMapper dozerMapper; //behavior
 
     private Person entity, person2;
-    private PersonVO personVO, personVO1;
+    private PersonVO personV1, personV2;
 
     @BeforeEach
     public void setUp() {
@@ -49,26 +49,27 @@ class PersonRepositoryTest {
         person2.setAdress("Cogumelo");
         person2.setGender("Male");
 
-        personVO1 = new PersonVO();
-        personVO1.setKey(1L);
-        personVO1.setFirstName("Mario");
-        personVO1.setLastName("Kart");
-        personVO1.setAdress("Cogumelo");
-        personVO1.setGender("Male");
 
-        personVO = new PersonVO();
-        personVO.setKey(2L);
-        personVO.setFirstName("Pingu");
-        personVO.setLastName("Pingado");
-        personVO.setAdress("Alasca");
-        personVO.setGender("Male");
+        personV1 = new PersonVO();
+        personV1.setKey(1L);
+        personV1.setFirstName("Pingu");
+        personV1.setLastName("Pingado");
+        personV1.setAdress("Alasca");
+        personV1.setGender("Male");
+
+        personV2 = new PersonVO();
+        personV2.setKey(2L);
+        personV2.setFirstName("Mario");
+        personV2.setLastName("Kart");
+        personV2.setAdress("Cogumelo");
+        personV2.setGender("Male");
     }
 
 
     @Test
     public void getAll() {
         List<Person> personList = Arrays.asList(entity);
-        List<PersonVO> personVOS = Arrays.asList(personVO);
+        List<PersonVO> personVOS = Arrays.asList(personV1);
 
         // configurando o comportamento do mock
         when(personRepository.findAll()).thenReturn(personList);
@@ -93,7 +94,7 @@ class PersonRepositoryTest {
         Long idToFind = 2L;
 
         when(personRepository.findById(idToFind)).thenReturn(Optional.of(person2));
-        when(dozerMapper.parseObject(person2, PersonVO.class)).thenReturn(personVO1);
+        when(dozerMapper.parseObject(person2, PersonVO.class)).thenReturn(personV2);
 
         PersonVO result = personServices.findById(idToFind);
 
@@ -105,5 +106,55 @@ class PersonRepositoryTest {
 
         verify(personRepository, times(1)).findById(idToFind);
     }
+
+
+
+    @Test
+    public void createEntity() {
+
+        when(dozerMapper.parseObject(any(PersonVO.class), eq(Person.class))).thenReturn(entity);
+
+        when(personRepository.save(any(Person.class))).thenAnswer(invocation -> { //id autoIncrement db
+            Person savedEntity = invocation.getArgument(0);
+            savedEntity.setId(1L);
+            return savedEntity;
+        });
+
+        when(dozerMapper.parseObject(any(Person.class), eq(PersonVO.class))).thenReturn(personV1);
+        PersonVO result = personServices.create(personV1);
+
+
+        assertNotNull(result);
+        assertEquals("Pingu", result.getFirstName());
+        assertEquals("Pingado", result.getLastName());
+        assertEquals("Alasca", result.getAdress());
+        assertEquals("Male", result.getGender());
+
+
+        verify(personRepository, times(1)).save(any(Person.class));
+        verify(dozerMapper, times(1)).parseObject(any(PersonVO.class), eq(Person.class));
+    }
+
+    @Test
+    public void updateEntity(){
+
+        Long idToFind = 2L;
+
+        when(personRepository.findById(idToFind)).thenReturn(Optional.of(entity));
+        when(dozerMapper.parseObject(entity, PersonVO.class)).thenReturn(personV1);
+
+        PersonVO result = personServices.findById(idToFind);
+        result.setFirstName("Marcos");
+        result.setLastName("Vinicius");
+        result.setAdress("Sao Paulo");
+        result.setGender("Male");
+
+        verify(personRepository, times(1)).findById(idToFind);
+    }
+
+
+
+
+
 
 }
