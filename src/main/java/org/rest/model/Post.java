fd4e05f52cd298ad51d26f27a4c83c84d49c9820
patricio0022 <niellucsx@hotmail.com>
2024-10-20 18:10:05 -0300

    new entity

--- src/main/java/org/rest/model/Post.java
+++ src/main/java/org/rest/model/Post.java
@@ -1,4 +1,56 @@
 package org.rest.model;
 
-public class Post {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Post implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title" , nullable = false)
+    private String title;
+    @Column(name = "body" , nullable = false)
+    private String body;
+
+    public Long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id) && Objects.equals(title, post.title) && Objects.equals(body, post.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, body);
+    }
 }
