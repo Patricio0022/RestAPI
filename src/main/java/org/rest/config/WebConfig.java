1e0e24ebdd724889b1d5defa76ea02d519fda957
patricio0022 <niellucsx@hotmail.com>
2024-10-20 17:08:32 -0300

    modified

--- src/main/java/org/rest/config/WebConfig.java
+++ src/main/java/org/rest/config/WebConfig.java
@@ -1,10 +1,10 @@
 package org.rest.config;
 
-
 import org.rest.serializationConverter.yamlHttpConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,29 +17,29 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-
-        //choose response via QueryParam http://localhost:8080/api/person/v1?mediaType=xml
-/*
+        // choose response via QueryParam http://localhost:8080/api/person/v1?mediaType=xml
+        /*
         configurer.favorParameter(true)
                 .parameterName("mediaType").ignoreAcceptHeader(true)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                     .mediaType("json", MediaType.APPLICATION_JSON)
-                    .mediaType("xml", MediaType.APPLICATION_XML); */
+                    .mediaType("xml", MediaType.APPLICATION_XML);
+        */
 
-       //choose via header param
+        // choose via header param
         configurer.favorParameter(false)
                 .ignoreAcceptHeader(false)
-                .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
+                .useRegisteredExtensionsOnly(false)
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
                 .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
-
     }
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(0, new MappingJackson2HttpMessageConverter());
         converters.add(new yamlHttpConverter());
-    }
+}
 }
