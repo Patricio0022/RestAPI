a2b519b4ddd45a7703253c90c1df5946de2e9a0f
patricio0022 <niellucsx@hotmail.com>
2024-10-20 21:16:39 -0300

    .

--- src/main/java/org/rest/RestApplication.java
+++ src/main/java/org/rest/RestApplication.java
@@ -3,12 +3,10 @@ package org.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.rest")
 public class RestApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(RestApplication.class, args);
-
-		}
+	}
 }
+
