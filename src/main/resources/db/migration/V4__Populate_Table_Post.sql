54682263847fc2ff47d9bc1188b239f90f75f6dc
patricio0022 <niellucsx@hotmail.com>
2024-10-20 21:19:03 -0300

    .

--- src/main/resources/db/migration/V4__Populate_Table_Post.sql
+++ src/main/resources/db/migration/V4__Populate_Table_Post.sql
@@ -1,3 +1,3 @@
-INSERT INTO `post` (user_id, title, body) VALUES
+INSERT INTO `post` (person_id, title, body) VALUES
 (1, 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
  'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto');
