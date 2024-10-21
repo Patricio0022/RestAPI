09f24c1bdfee00c137266b2814f381984f4d2090
patricio0022 <niellucsx@hotmail.com>
2024-10-20 21:16:54 -0300

    new controller

--- src/main/java/org/rest/controller/PostController.java
+++ src/main/java/org/rest/controller/PostController.java
@@ -1,7 +1,6 @@
 package org.rest.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -15,116 +14,81 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-    @RestController
-    @CrossOrigin(origins = "*", allowedHeaders = "*") //cors
-    @RequestMapping(value = "/api/Post/v1", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Tag(name = "Post", description = "Endpoints For Managing Post") //swagger config
-    public class PostController {
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*") // CORS
+@RequestMapping(value = "/api/post/v1", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "post", description = "Endpoints for managing posts") // Swagger config
+public class PostController {
 
-        @Autowired //dependencies injection
-        private PostService service;
+    @Autowired
+    private PostService postService;
 
-        //Https verbs
-        @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml"})
-        //swagger
-        @Operation(summary = "Finds A Post", description = "Finds A Post", tags = {"Post"},
-                responses = {
-                        @ApiResponse(description = "Sucess", responseCode = "200",
-                                content = @Content(schema = @Schema(implementation = PostVO.class))
-                        ),
-                        @ApiResponse(description = "Not content", responseCode = "204", content = @Content),
-                        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                        @ApiResponse(description = "Not Found", responseCode = "401", content = @Content),
-                        @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
-                }
-        )
-        public List<PostVO> findAll() {
-            return service.findAll();
-        }
-
-        @GetMapping(value = "/{id}",
-                produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml" })
-
-        @Operation (summary = "Find Post", description = "Find Post", tags = {"Post"},
-                responses = {
-                        @ApiResponse(description = "Sucess", responseCode = "200",
-                                content = {
-                                        @Content(
-                                                mediaType = "application/json",
-                                                array = @ArraySchema(schema = @Schema(implementation = PostVO.class))
-                                        )
-                                }),
-                        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                        @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-                        @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
-
-                })
-        public PostVO findById(@PathVariable(value = "id") Long id) {
-            return service.findById(id);
-        }
-
-
-        @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,"application/x-yaml" } ,
-                produces = { MediaType.APPLICATION_JSON_VALUE , MediaType.APPLICATION_XML_VALUE,"application/x-yaml"})
-
-        @Operation(summary = "Adds a new Post",
-                description = "Adds a new Post by passing in a JSON, XML or YML representation of the Post!",
-                tags = {"Post"},
-                responses = {
-                        @ApiResponse(description = "Success", responseCode = "200",
-                                content = @Content(schema = @Schema(implementation = PostVO.class))
-                        ),
-                        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-                }
-        )
-        public PostVO create(@RequestBody PostVO Post) {
-            return service.create(Post);
-        }
-
-
-        @PutMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE ,"application/x-yaml"} ,
-                produces = { MediaType.APPLICATION_JSON_VALUE , MediaType.APPLICATION_XML_VALUE,"application/x-yaml"})
-
-        @Operation(summary = "Updates a Post",
-                description = "Updates a Post by passing in a JSON, XML or YML representation of the Post!",
-                tags = {"Post"},
-                responses = {
-                        @ApiResponse(description = "Updated", responseCode = "200",
-                                content = @Content(schema = @Schema(implementation = PostVO.class))
-                        ),
-                        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                        @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-                        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-                }
-        )
-        public PostVO update(@RequestBody PostVO Post) {
-            return service.update(Post);
-        }
-
-
-        @DeleteMapping(value = "/{id}")
-
-        @Operation(summary = "Deletes a Post",
-                description = "Deletes a Post by passing in a JSON, XML or YML representation of the Post!",
-                tags = {"Post"},
-                responses = {
-                        @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
-                        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                        @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-                        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-                }
-        )
-        public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
-            service.delete(id);
-            return ResponseEntity.noContent().build();
-        }
-
-
+    @Operation(summary = "Test Endpoint", description = "This endpoint is for testing purposes.")
+    @ApiResponse(responseCode = "200", description = "Successful response")
+    @GetMapping("/get")
+    public String testEndpoint() {
+        return "Hello, this is a test endpoint!";
     }
 
+    @GetMapping
+    @Operation(summary = "Find all posts", description = "Retrieves a list of all posts", tags = {"Post"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PostVO.class))),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            })
+    public List<PostVO> findAll() {
+        return postService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostVO> findById(@PathVariable Long id) {
+        PostVO postVO = postService.findById(id);
+        return ResponseEntity.ok(postVO);
+    }
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml"})
+    @Operation(summary = "Adds a new post", description = "Adds a new post by passing in a JSON, XML or YML representation of the post!",
+            tags = {"Post"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "201",
+                            content = @Content(schema = @Schema(implementation = PostVO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
+    public PostVO create(@RequestBody PostVO post) {
+        return postService.create(post);
+    }
+
+    @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, "application/x-yaml"})
+    @Operation(summary = "Updates a post", description = "Updates a post by passing in a JSON, XML or YML representation of the post!",
+            tags = {"Post"},
+            responses = {
+                    @ApiResponse(description = "Updated", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PostVO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
+    public PostVO update(@RequestBody PostVO post) {
+        return postService.update(post);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Deletes a post", description = "Deletes a post by ID", tags = {"Post"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
+    public ResponseEntity<Void> delete(@PathVariable(value = "id") Long id) {
+        postService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
