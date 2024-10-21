30ace86a16965a6a3cc08ab3afd861ca5d04df63
patricio0022 <niellucsx@hotmail.com>
2024-10-20 21:17:39 -0300

    new entity

--- src/main/java/org/rest/model/Post.java
+++ src/main/java/org/rest/model/Post.java
@@ -8,23 +8,30 @@ import java.util.Objects;
 @Entity
 public class Post implements Serializable {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "title" , nullable = false)
+    @Column(name = "title", nullable = false)
     private String title;
-    @Column(name = "body" , nullable = false)
+
+    @Column(name = "body", nullable = false)
     private String body;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", nullable = false)
+    private Person person;
 
     public Long getId() {
         return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
@@ -41,16 +48,28 @@ public class Post implements Serializable {
         this.body = body;
     }
 
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Post post = (Post) o;
-        return Objects.equals(id, post.id) && Objects.equals(title, post.title) && Objects.equals(body, post.body);
-    }
+        return Objects.equals(id, post.id) &&
+                Objects.equals(title, post.title) &&
+                Objects.equals(body, post.body) &&
+                Objects.equals(person, post.person); }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, body);
+        return Objects.hash(id, title, body, person);
     }
+
+
 }
